de1dd9591120e1361864f46b308eaca2678cab2c
arjunsingrajput01 <arjunsingrajput01@gmail.com>
2023-11-10 13:16:09 +0530

    Initial Commit.

---
+++ src/FindPeakElement162.java
@@ -0,0 +1,20 @@
+public class FindPeakElement162 {
+    public int findPeakElement(int[] arr) {
+        int start=0;
+        int end=arr.length-1;
+
+        while(start<end){
+            int mid= start+ (end-start)/2;
+            if(arr[mid]>arr[mid+1]){
+                //there may be a greater element than the arr[mid] on the left thats why end=mid instead of end=mid-1
+                end=mid;
+            }
+            else{
+                start=mid+1;
+            }
+        }
+
+        return start;
+    }
+
+}
